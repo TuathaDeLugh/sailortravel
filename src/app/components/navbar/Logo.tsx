1e9fc7fbf7054af3cc112381e48f41d4b9109fdd
Umang Sailor <ursailor@gmail.com>
2023-05-05 19:49:14 +0530

    jsut added header

---
+++ src/app/components/navbar/Logo.tsx
@@ -0,0 +1,27 @@
+'use client';
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+const Logo = () => {
+    const router = useRouter();
+    return (
+        <div>
+
+        {/* <Image
+        alt="logo"
+        className="hidden
+        md:block
+          cursor-pointer"
+        height="70"
+        width="70"
+        src="/images/sitelogo.png"/> */}
+        <label className="hidden
+        md:block
+        text-3xl
+        font-sans
+        font-bold
+          cursor-pointer">Sailor Travel</label>
+        </div> 
+     );
+}
+ 
+export default Logo;
